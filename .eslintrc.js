7097cc08cc8cc6c11984f2ac809ed8cd835f0648
auliaa <Jeffersonsimbiak995@gmail.com>
2025-04-30 22:51:35 +0800

    hapus isi lama

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false,
-  },
-  extends: ['@react-native'],
-};
--- App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import EnterQuiz from './EnterQuiz'; // Pastikan path dan huruf besar kecil pas
-
-export default function App() {
-  return <EnterQuiz />;
-}

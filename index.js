a540fa08cee4abe3808d7c376ec827a93fd5db76
auliaa <Jeffersonsimbiak995@gmail.com>
2025-04-30 20:21:11 +0800

    Resolve merge conflicts

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import AboutUs from '@pages/AboutUs';
-import MyProfil from '@pages/MyProfile';
-import editpass from '@pages/editpass';
-import Home from '@pages/Home';
-import Report from '@pages/Report';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Report);
+import App from './App';
+AppRegistry.registerComponent(appName, () => App);

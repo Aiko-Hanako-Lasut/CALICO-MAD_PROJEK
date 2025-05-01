81264dfaf2c2889f8273283fe6d4d451eb075850
Syeela Putri <s22310495@student.unklab.ac.id>
2025-05-01 13:13:36 +0800

    clean project for replacing syeela

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import Slicing from './Slicing';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Slicing);
